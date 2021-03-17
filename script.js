01e2787c7703c7cf313fe1704e30fd5b320be250
Steve Dever <stephendever.cr@gmail.com>
2021-03-16 21:05:42 -0400

    insert base randomizing code

--- script.js
+++ script.js
@@ -1,51 +1,81 @@
 var size  
-var caseL 
-var caseU
-var num 
-var schar
+var caseL = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var caseU = ["A","B","C","D","E","F","G","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var num = ["0","1","2","3","4","5","6","7","8","9"];
+var schar = ["!","@","#","%","^","&","*","$","(",")"];
 var i = 0
 var d = 0
-//is a "while" or "for" statement needed to control loop for first question ??
+var wantsLow
+var wantsUp
+var wantsNum
+var wanstSchar
 
-size = window.prompt("What length do you prefer (between 8 and 128?");
+// Add event listener to generate button
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword(); 
+var passwordText = document.querySelector("#password"); 
+passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+var password = ""
+
+size = window.prompt("What length do you prefer between 8 and 128?");
 if (size < 129 && size > 7) {
     console.log(size); }
 
-else if (size > 128 || size < 8) {
- size = window.prompt("Please pick a length between 8 and 128."); 
-    console.log(size); }
+//else if (size > 128 || size < 8) {
+// size = window.alert("Your length was not between 8 and 128. Please start over."); 
+//     return;
+//           }
 
-
-else (size.isNaN)
-    (window.prompt("This is not a number. Please enter a number from between 8 and 128."));
-     console.log(size);  // bombs out if not a number ---- displays prompt but then says
-     // NAN is not a function        
-                
-// Add code to repeat the question if it doesn't validate between 8 and 128
-// or is NULL or is a letter or symbol
-
+//else {(isNaN) 
+//    (window.alert("This is not a number. Please start over."));{
+//     return;            }
     
 
-caseL = window.confirm ('Do you want lower case?'); {
-    if (true) {(d=(d+1))}  
-    console.log(caseL);}
+wantsLow = window.confirm ('Do you want lower case?'); {
+    if (true) {
+        password+=caseL[Math.floor(Math.random()*caseL.length)];    
+        (d=(d+1)) }   
+}
 
-caseU = window.confirm ('Do you want upper case?'); {
-    if (true) {(d=(d+1))}
-    console.log(caseU); }
+wantsUp = window.confirm ('Do you want upper case?'); {
+    if (true) {
+        password+=caseU[Math.floor(Math.random()*caseU.length)];    
+        (d=(d+1)) }
+}
 
-num = window.confirm ('Do you want to include a number?'); {
-    if (true) {(d=(d+1))}    
-    console.log(num); }
+wantsNum = window.confirm ('Do you want to include a number?'); {
+    if (true) {
+        password+=num[Math.floor(Math.random()*num.length)];    
+        (d=(d+1)) }
+}
+    
+wanstSchar = window.confirm ('Do you want to include a special character?'); {
+    if (true) {
+        password+=schar[Math.floor(Math.random()*schar.length)];    
+        (d=(d+1)) }
+}
+//if (d < 1) (window.alert("You didn't select any details. Please start over.")); {
+//    return;
+//} 
+    // how to stop the program here?
 
-schar = window.confirm ('Do you want to include a special character?'); {
-    if (true) {(d=(d+1))}        
-    console.log(schar);}
+//allChoices = ["caseL" + "caseU" + "num" + "schar"];
+//console.log(allChoices);
 
-    console.log(d); // This is a counter to make sure at least one option
-                    // in addition to size (length) was chosen. If d < 1 
-                    // at this point, the last 4 questions should be re-asked 
-                    // since (I believe) one choice is required. (?)
+return password;
+                }
+
+// size: "length" --> exact number of characters for random string
 
 
 // while statement here? i.e.:
@@ -53,30 +83,9 @@ schar = window.confirm ('Do you want to include a special character?'); {
 // while i < size.length { run loop x number of iteration based on the number (size)
 // the user wants his/her password to be?
 
+
 // do you need to go through multiple iterations to make sure you capture at least 1
 // of each item selected by the user's prompt inputs?
 
 
-// size: "length" --> exact number of characters for random string
-// if caseL=true, caseL = "abcdefghijklmnopqrstuvwxyz"           
-// if caseU=true, caseU="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// if num=true, num="0123456789"
-// if schar=true, schar= " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
